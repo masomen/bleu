d25bf6afee651eb0fab8a1fa63cf7fd4ddc177b7
masomen <altun@att.net>
2024-12-16 19:57:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Function to copy the contract address
-function copyContractAddress() {
-  const address = document.getElementById("contract-address-text").innerText;
-  navigator.clipboard.writeText(address).then(() => {
-    alert("Contract Address Copied!");
-  }).catch(err => {
-    console.error("Failed to copy text: ", err);
-    alert("Failed to copy. Please try again!");
-  });
-}
+<script>
+  // Function to copy the contract address
+  function copyContractAddress() {
+    const address = document.getElementById("contract-address").innerText.split(": ")[1];
+    navigator.clipboard.writeText(address).then(() => {
+      alert("Contract Address Copied!");
+    });
+  }
+</script>
